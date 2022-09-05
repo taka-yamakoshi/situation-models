aa2b5485fd00ed530f2051d63cc4022900db4f4f
taka-yamakoshi <takateru@vpn12-client-172-20-209-3.princeton.edu>
2022-09-05 13:04:34 +0900

    remove limits

--- code/create_dataframe.py
+++ code/create_dataframe.py
@@ -28,16 +28,16 @@ if __name__ =='__main__':
 
     dataset_name = dataset + f'_{size}' if dataset == 'winogrande' else dataset
 
-    pos_types_singles = ['options','context','masks','verb','period','cls-sep','rest']
+    pos_types_singles = ['options','masks'] #['options','context','masks','verb','period','cls-sep','rest']
     pos_types_q_and_k = ['options-masks','masks-options','context-masks','context-options']
     cue_type_list = ['context','verb','context_verb','synonym_1','synonym_2']
     stimuli_list = ['original','control','control','synonym_1','synonym_2']
     mask_context_list = [False,True,False,False,False]
-    rep_types = ['layer-query-key-value','z_rep','z_rep','value', 'attention'] #,'q_and_k']
-    cascade_list = [False,True,False,False,False] #,False]
-    multihead_list = [True,True,False,False,False] #,False]
+    rep_types = ['layer-query-key-value'] #['layer-query-key-value','z_rep','z_rep','value', 'attention','q_and_k']
+    cascade_list = [False] #[False,True,False,False,False,False]
+    multihead_list = [True] #[True,True,False,False,False,False]
 
-    os.makedirs(f'{os.environ.get("MY_DATA_PATH")}/intervention/tmp/combined/',exist_ok=True)
+    os.makedirs(f'{os.environ.get("MY_DATA_PATH")}/intervention/combined/',exist_ok=True)
     for rep_type,cascade,multihead in zip(rep_types,cascade_list,multihead_list):
         df = pd.DataFrame([])
         cascade_id = '_cascade' if cascade else ''
@@ -45,7 +45,7 @@ if __name__ =='__main__':
         for stimuli,mask_context,cue_type in zip(stimuli_list,mask_context_list,cue_type_list):
             mask_context_id = '_mask_context' if mask_context else ''
             if rep_type=='attention':
-                file_name = f'{os.environ.get("MY_DATA_PATH")}/intervention/tmp/{dataset_name}_{stimuli}{mask_context_id}_intervention_swap_'\
+                file_name = f'{os.environ.get("MY_DATA_PATH")}/intervention/{dataset_name}_{stimuli}{mask_context_id}_intervention_swap_'\
                                 +f'None_{rep_type}_{model}_layer_all_head_all{cascade_id}{multihead_id}.csv'
                 loaded_df = loaded_df_preprocess(file_name,show_last_mod=True,
                                                  cols=['pair_id','sent_1','sent_2','layer_id','head_id','original_score',metric])
@@ -54,10 +54,10 @@ if __name__ =='__main__':
             else:
                 pos_types = pos_types_q_and_k if rep_type=='q_and_k' else pos_types_singles
                 for pos_type in pos_types:
-                    file_name = f'{os.environ.get("MY_DATA_PATH")}/intervention/tmp/{dataset_name}_{stimuli}{mask_context_id}_intervention_swap_'\
+                    file_name = f'{os.environ.get("MY_DATA_PATH")}/intervention/{dataset_name}_{stimuli}{mask_context_id}_intervention_swap_'\
                                     +f'{pos_type}_{rep_type}_{model}_layer_all_head_all{cascade_id}{multihead_id}.csv'
                     loaded_df = loaded_df_preprocess(file_name,show_last_mod=True,
                                                      cols=['pair_id','sent_1','sent_2','layer_id','head_id','original_score',metric])
                     loaded_df = loaded_df.assign(cue_type=cue_type,pos_type=pos_type,rep_type=f'{rep_type}{cascade_id}{multihead_id}')
                     df = pd.concat([df,loaded_df])
-        df.to_csv(f'{os.environ.get("MY_DATA_PATH")}/intervention/tmp/combined/{dataset}_{metric}_{rep_type}{cascade_id}{multihead_id}.csv',index=False)
+        df.to_csv(f'{os.environ.get("MY_DATA_PATH")}/intervention/combined/{dataset}_{metric}_{rep_type}{cascade_id}{multihead_id}.csv',index=False)
